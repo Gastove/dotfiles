45179aad690c09d0ab69d1b905b41af51904eb7c
Ross Donaldson <gastove@gmail.com>
2014-12-11 19:49:53 -0800

    Add a lein profile to extras

---
+++ extra/profiles.clj
@@ -0,0 +1,8 @@
+{:user
+ {:plugins [[cider/cider-nrepl "0.8.2-SNAPSHOT"]
+            [lein-plz "0.2.1"]
+            [lein-pprint "1.1.1"]
+            [codox "0.8.10"]]
+  :dependencies [[alembic "0.2.1"]
+                 [slamhound "1.5.5"]]
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
