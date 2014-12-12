d3bc0949df4144e7665d565d2bd3e6eb27beafc2
Ross Donaldson <gastove@gmail.com>
2014-12-11 20:10:46 -0800

    Change the way the lein symlink is done; update lein plugins.

--- extra/profiles.clj
+++ extra/profiles.clj
@@ -2,7 +2,8 @@
  {:plugins [[cider/cider-nrepl "0.8.2-SNAPSHOT"]
             [lein-plz "0.2.1"]
             [lein-pprint "1.1.1"]
-            [codox "0.8.10"]]
+            [codox "0.8.10"]
+            [refactor-nrepl "0.2.2"]]
   :dependencies [[alembic "0.2.1"]
                  [slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
