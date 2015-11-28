fc67cf96359995a5236638377cf531cc671ca0c6
Ross Donaldson <gastove@gmail.com>
2015-11-27 23:01:53 -0800

    Upgrade nrepl version for cider

--- extra/profiles.clj
+++ extra/profiles.clj
@@ -6,5 +6,5 @@
             [refactor-nrepl "2.0.0-SNAPSHOT"]]
   :dependencies [[alembic "0.2.1"]
                  [slamhound "1.5.5"]
-                 [org.clojure/tools.nrepl "0.2.7"]]
+                 [org.clojure/tools.nrepl "0.2.12"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
