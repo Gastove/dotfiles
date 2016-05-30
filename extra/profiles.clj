47e54c830adac6b3239ea7d91da5927307d84e4a
Ross Donaldson <gastove@gmail.com>
2016-05-29 17:43:10 -0700

    bump refactor-nrepl version

--- extra/profiles.clj
+++ extra/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
             [lein-pprint "1.1.1"]
-            [refactor-nrepl "2.0.0-SNAPSHOT"]
+            [refactor-nrepl "2.3.0-SNAPSHOT"]
             [venantius/ultra "0.4.1"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
